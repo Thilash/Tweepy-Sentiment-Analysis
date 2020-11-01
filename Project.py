b7295cbae514f667ee86c077f8c2d298d1accab6
Thilash Yohathasan <47063498+Thilash@users.noreply.github.com>
2020-11-01 01:53:00 -0500

    Sentiment Analysis

--- Project.py
+++ Project.py
@@ -2,7 +2,7 @@ import tweepy
 import webbrowser
 import time
 from textblob import TextBlob
-import preprocessor as p
+import preprocessor
 import statistics
 from typing import List
 
@@ -20,22 +20,22 @@ print(me.screen_name)
 
 def get_tweets(keyword: str) -> List[str]:
     tweets = []
-    for tweet in tweepy.Cursor(api.search, q=keyword, tweet_mode='extended', lang='en').items(15):
+    for tweet in tweepy.Cursor(api.search, q=keyword, tweet_mode='extended', lang='en').items(15): #takes 15 tweets from the timeline with the keyword only text
         tweets.append(tweet.full_text)
     return tweets
 
 def clean(tweets: List[str]) -> List[str]:
     clean_tweets = []
     for tweet in tweets:
-        clean_tweets.append(p.clean(tweet))
+        clean_tweets.append(preprocesssor.clean(tweet)) #cleans out all the tweets. gets rid of urls, photos, hashtags
     return clean_tweets
 
 def sentiment(tweets: List[str]) -> List[float]:
     review = []
     for tweet in tweets:
-        blob = TextBlob(tweet)
-        review.append(blob.sentiment.polarity)
-    return review
+        blob = TextBlob(tweet) #creates a textblob object of the tweet
+        review.append(blob.sentiment.polarity) #textblob is a library that does word processing. returns polarity and subjecttivity of each word. 
+    return review #-0.3 would be given to "not a great word"
 
 def my_mean(values: List[float]) -> int:
    sum, n = 0, 0
@@ -47,7 +47,7 @@ def my_mean(values: List[float]) -> int:
 def avg_sentiment(keyword) -> int:
     tweets = get_tweets(keyword)
     clean_tweets = clean(tweets)
-    sentimental = sentiment(clean_tweets)
+    sentimental = sentiment(clean_tweets) 
     avg = my_mean(sentimental)
     return avg
 
